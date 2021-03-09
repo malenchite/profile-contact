5e31562de5d234d87e7561938e930c54edea14c9
Steven Israel <malenchite@gmail.com>
2021-03-08 21:22:46 -0500

    Fixed a few minor linting errors

--- index.js
+++ index.js
@@ -2,36 +2,14 @@ const nodemailer = require("nodemailer");
 const express = require("express");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 const corsOptions = {
   origin: process.env.PERMITTEDHOSTS.split(" "),
   optionsSuccessStatus: 200
-}
-
-/* Middleware */
-app.use(cors(corsOptions));
-app.use(express.json());
-
-/* Primary API route */
-app.post("/contact", (req, res) => {
-  if (!("name" in req.body)) {
-    res.status(400).send("Name required");
-  } else if (!("email" in req.body)) {
-    res.status(400).send("Email required");
-  } else if (!("message" in req.body)) {
-    res.status(400).send("Message required");
-  } else {
-    sendMail(req.body.name, req.body.email, req.body.message)
-      .then(() => res.status(200).send("SUCCESS"))
-      .catch((err) => res.status(502).send({ err }));
-  }
-});
-
-/* Start server */
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+};
 
 /* Sends email to Yahoo SMTP server */
 function sendMail(name, email, message) {
@@ -57,3 +35,26 @@ function sendMail(name, email, message) {
     Email: ${email}`
   });
 }
+
+/* Middleware */
+app.use(cors(corsOptions));
+app.use(express.json());
+
+/* Primary API route */
+app.post("/contact", (req, res) => {
+  if (!("name" in req.body)) {
+    res.status(400).send("Name required");
+  } else if (!("email" in req.body)) {
+    res.status(400).send("Email required");
+  } else if (!("message" in req.body)) {
+    res.status(400).send("Message required");
+  } else {
+    sendMail(req.body.name, req.body.email, req.body.message)
+      .then(() => res.status(200).send("SUCCESS"))
+      .catch((err) => res.status(502).send({ err }));
+  }
+});
+
+/* Start server */
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
